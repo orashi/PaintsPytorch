c5ef226da501dfcfdb24906a66728bee2bc4359a
Yuanzheng Ci <yuanzheng.ci@gmail.com>
2017-07-18 20:55:40 +0800

    return to hint and eval proxy

--- models/naive_model.py
+++ models/naive_model.py
@@ -199,6 +199,9 @@ class NLayerDiscriminator(nn.Module):
         padw = 1
         self.ndf = ndf
 
+        down = [nn.Conv2d(4, ndf, kernel_size=3, stride=1, padding=1), norm_layer(ndf)]
+        self.downH = nn.Sequential(*down)
+
         sequence = [
             nn.Conv2d(4, ndf, kernel_size=kw, stride=2, padding=padw),
             nn.LeakyReLU(0.2, True)
@@ -210,9 +213,10 @@ class NLayerDiscriminator(nn.Module):
             norm_layer(ndf * 2),
             nn.LeakyReLU(0.2, True)
         ]
+        self.model = nn.Sequential(*sequence)
 
-        sequence += [
-            nn.Conv2d(ndf * 2, ndf * 4,
+        sequence = [
+            nn.Conv2d(ndf * 3, ndf * 4,
                       kernel_size=kw, stride=2, padding=padw),
             norm_layer(ndf * 4),
             nn.LeakyReLU(0.2, True)
@@ -226,18 +230,19 @@ class NLayerDiscriminator(nn.Module):
             nn.Conv2d(ndf * 8, 1, kernel_size=kw, stride=1, padding=padw)
         ]
 
-        self.model = nn.Sequential(*sequence)
+        self.model2 = nn.Sequential(*sequence)
 
         LR_weight_init(self)
 
-    def forward(self, input):
-        return self.model(input)
-    
+    def forward(self, input, hint):
+        v = F.leaky_relu(self.downH(hint), 0.2, True)
+        temp = self.model(input)
+        return self.model2(torch.cat([temp, v], 1))
 
 
 def def_netF():
     vgg16 = M.vgg16()
-    vgg16.load_state_dict(torch.load('vgg16.pth'))
+    vgg16.load_state_dict(torch.load('vgg16-397923af.pth'))
     vgg16.features = nn.Sequential(
         *list(vgg16.features.children())[:9]
     )
