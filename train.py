826466d02882d785fbaa9ace7a6c60214213c96c
Yuanzheng Ci <yuanzheng.ci@gmail.com>
2017-11-24 15:52:55 +0800

    :bug: fix probability bug

--- train.py
+++ train.py
@@ -141,7 +141,7 @@ def calc_gradient_penalty(netD, real_data, fake_data):
 
 flag = 1
 lower, upper = 0, 1
-mu, sigma = 1, 0.01
+mu, sigma = 1, 0.0012
 maskS = opt.imageSize // 2
 X = stats.truncnorm(
     (lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma)
