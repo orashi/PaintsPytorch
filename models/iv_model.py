27bbedbbead2659a2443ccefa6e1114f7b658d04
Yuanzheng Ci <yuanzheng.ci@gmail.com>
2018-01-20 13:48:02 +0800

    :bug: fix bug

--- models/iv_model.py
+++ models/iv_model.py
@@ -8,7 +8,6 @@ from torch.autograd import Variable
 from torch.nn import init
 import torch.nn.functional as F
 import torchvision.models as M
-from .i2v import i2v
 
 VGG16_PATH = 'vgg16-397923af.pth'
 I2V_PATH = 'i2v.pth'
@@ -203,7 +202,38 @@ class def_netF(nn.Module):
 class def_netI(nn.Module):
     def __init__(self):
         super(def_netI, self).__init__()
-        i2v_model = i2v()
+        i2v_model = nn.Sequential(  # Sequential,
+            nn.Conv2d(3, 64, (3, 3), (1, 1), (1, 1)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+            nn.Conv2d(64, 128, (3, 3), (1, 1), (1, 1)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+            nn.Conv2d(128, 256, (3, 3), (1, 1), (1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, (3, 3), (1, 1), (1, 1)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+            nn.Conv2d(256, 512, (3, 3), (1, 1), (1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, (3, 3), (1, 1), (1, 1)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+            nn.Conv2d(512, 512, (3, 3), (1, 1), (1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, (3, 3), (1, 1), (1, 1)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
+            nn.Conv2d(512, 1024, (3, 3), (1, 1), (1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(1024, 1024, (3, 3), (1, 1), (1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(1024, 1024, (3, 3), (1, 1), (1, 1)),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Conv2d(1024, 1539, (3, 3), (1, 1), (1, 1)),
+            nn.AvgPool2d((7, 7), (1, 1), (0, 0), ceil_mode=True),  # AvgPool2d,
+        )
         i2v_model.load_state_dict(torch.load(I2V_PATH))
         i2v_model = nn.Sequential(
             *list(i2v_model.children())[:15]
